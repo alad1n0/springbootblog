0043668bef8ee7aaef5cdcbfe2dd0d408048e847
alad1n0 <alad1nfaceit.al@gmail.com>
2024-09-14 19:06:04 +0300

    addGlobalUpdate

--- src/main/java/edu/itstep/final_project_v1/web/config/WebSecurityConfig.java
+++ src/main/java/edu/itstep/final_project_v1/web/config/WebSecurityConfig.java
@@ -2,15 +2,10 @@ package edu.itstep.final_project_v1.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
